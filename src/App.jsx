d2a94ed4a23ee61fa723a526b517fd6d0e52663a
Sharon-liu-go <innovate72095@gmail.com>
2023-05-25 08:40:51 +0800

    feat: 導入mui

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage, SignUpPage, HomePage } from './pages';
 function App() {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="home" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+    <div>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            {/* <Route path="home" element={<HomePage />} /> */}
+          </Routes>
+        </BrowserRouter>
+      </div>
+
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="home" element={<HomePage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
+
   );
 }
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,14 +6,61 @@ import {
 } from '../components/common/auth.styled';
 import { ACLogoIcon } from '../assets/images';
 import { AuthInput } from '../components';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuItem from '@mui/material/MenuItem';
+import MenuList from '@mui/material/MenuList';
+import Stack from '@mui/material/Stack';
 
 const HomePage = () => {
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
   return (
-    <AuthContainer>
-      <div>
-      </div>
-      <h1>WELCOME</h1>
-    </AuthContainer>
+    <Stack direction="row" spacing={2}>
+
+      <MenuList>
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </MenuList>
+
+    </Stack>
   );
 };
 
